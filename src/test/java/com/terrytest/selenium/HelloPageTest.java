e6340017f7b27387d62f8b728f3bc589dad371e6
puppypower <terry.onlyyou@gmail.com>
2015-04-10 14:39:37 +0800

    Update hello world test case

--- src/test/java/com/terrytest/selenium/HelloPageTest.java
+++ src/test/java/com/terrytest/selenium/HelloPageTest.java
@@ -2,30 +2,37 @@ package com.terrytest.selenium;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by terry on 4/10/15.
  */
-public class HelloPageTest extends TestCase{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+public class HelloPageTest{
     private Selenium selenium;
 
-    @Override
+    @Before
     public void setUp() throws Exception {
         selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8181");
         selenium.start();
-        super.setUp();
     }
 
-    @Override
+    @After
     public void tearDown() throws Exception {
         selenium.stop();
-        super.tearDown();
     }
 
+    @Test
     public void testHelloMessage() {
         selenium.open("http://localhost:8181");
         selenium.waitForPageToLoad("1000");
-        assertEquals(selenium.getTitle(), "Hello");
+        Assert.assertEquals(selenium.getTitle(), "Hello");
     }
 }
